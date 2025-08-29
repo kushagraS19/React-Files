990e78bfe095979a5abb25ea20becb7ec76649f2
AnishS109 <anishsaini9098@gmail.com>
2025-08-29 23:24:34 +0530

    Bharat clock updated with useEffect

--- Projects/Bharat-clock/src/components/time.jsx
+++ Projects/Bharat-clock/src/components/time.jsx
@@ -1,6 +1,26 @@
-function CurrentTime(){
-  let time = new Date();
-  return <p className="lead">The current Time is : {time.toLocaleDateString()} - {time.toLocaleTimeString()} </p>;
-}
+import { useEffect, useState } from "react";
+
+const CurrentTime = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    console.log("set up");
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+      console.log("canceled");
+    };
+  });
+
+  return (
+    <p className="lead">
+      The current Time is : {time.toLocaleDateString()} -{" "}
+      {time.toLocaleTimeString()}{" "}
+    </p>
+  );
+};
 
 export default CurrentTime;
